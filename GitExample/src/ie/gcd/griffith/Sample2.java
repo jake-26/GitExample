d869703e1fef4396f04c9265887229342632adf2
Dude <jake.itb@live.com>
2013-04-10 13:26:47 +0100

    Finished working on branch

---
+++ GitExample/src/ie/gcd/griffith/Sample2.java
@@ -0,0 +1,16 @@
+package ie.gcd.griffith;
+
+public class Sample2 {
+	
+	public Sample2(){
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
