323e9c73c54a6a387377bc7ecd488381614a7847
Jake <jake.itb@live.com>
2013-04-10 13:13:23 +0100

    Initial Commit

---
+++ GitExample/src/ie/gcd/griffith/Sample.java
@@ -0,0 +1,16 @@
+package ie.gcd.griffith;
+
+public class Sample {
+
+	public Sample(){
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
